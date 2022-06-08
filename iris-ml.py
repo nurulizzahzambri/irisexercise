28b12df54e3346e29f41b35dc527d19cf29cdc92
nurulizzahzambri <106502769+nurulizzahzambri@users.noreply.github.com>
2022-06-08 16:27:47 +0800

    Update iris-ml.py

--- iris-ml.py
+++ iris-ml.py
@@ -27,7 +27,7 @@ df = user_input_features()
 st.subheader('User Input parameters')
 st.write(df)
 
-iris = datasets.load_iris()
+iris = pd.read_csv("./irisexercise/IRIS.csv")
 X = iris.data
 Y = iris.target
 
